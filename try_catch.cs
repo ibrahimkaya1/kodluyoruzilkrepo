d60d96987d4f80335f25d772e9cbfc86315b1791
ibrahimkaya <ibo_55_kaya@hotmail.com>
2022-07-31 16:38:44 +0300

    Try catch finally
    Try catch

---
+++ try_catch.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    static void Main(String[] args) {
+        
+        try
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = a+b; 
+            Console.WriteLine(c);
+        } 
+        catch(Exception ex)
+        {   
+            Console.WriteLine("Bir Hata Oluştu " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Operasyon Tamamlandı.");
+        }
+    
+    }
+}

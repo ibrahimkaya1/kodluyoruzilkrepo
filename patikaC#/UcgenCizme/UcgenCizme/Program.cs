69102b28cfa49db32fd97ae300e6213335fd226e
ibrahimkaya <ibo_55_kaya@hotmail.com>
2022-08-01 17:05:54 +0300

    patika trigon
    patika üçgen çizdirme Singleton

---
+++ patikaC#/UcgenCizme/UcgenCizme/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UcgenCizme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            trigon(n);
+        }
+
+        private static void trigon(int n)
+        {
+           for(int i = 0; i < n; i++)
+           {
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+           }
+        }
+    }
+}
